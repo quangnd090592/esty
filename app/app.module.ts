2ad13b831768ed39fda0b236083ca1da9b68412b
Nguyen Duy Quang <quangnd.92@gmail.com>
2016-10-05 19:21:57 +0700

    update

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 
 import { AppComponent }   from './app.component';
+import { RecentlyViewedComponent }   from './home/recently-viewed.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  declarations: [
-    AppComponent
-  ],
-  providers: [
-  ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+    ],
+    declarations: [
+        AppComponent,
+        RecentlyViewedComponent,
+    ],
+    providers: [
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule {
 }
---
+++ app/home/recently-viewed.component.ts
@@ -0,0 +1,11 @@
+import { Component }		from '@angular/core';
+
+@Component ({
+	moduleId: module.id,
+	selector : 'recently-viewed',
+	templateUrl : '../view/home/recently-viewed.component.html'
+})
+
+export class RecentlyViewedComponent {
+
+}
